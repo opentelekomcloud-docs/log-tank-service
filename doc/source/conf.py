655003ef70491fd2609df72b776ff1580589ffae
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2023-11-16 16:40:46 +0100

    Update tox.ini && conf.py file (#31)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Tino Schr

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,9 +34,11 @@ otcdocs_doc_environment = ''
 otcdocs_doc_link = ''
 otcdocs_doc_title = ''
 otcdocs_doc_type = ''
-otcdocs_service_title = ''
+otcdocs_service_category = 'md'
+otcdocs_service_title = 'Log Tank Service'
 otcdocs_service_type = 'lts'
-otcdocs_service_category = ''
+otcdocs_search_environment = 'hc_de'
+otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
